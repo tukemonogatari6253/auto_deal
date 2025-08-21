7f7560b84a74847f87d9dabf890390045b91afcb
tukemonogatari6253 <110277953+tukemonogatari6253@users.noreply.github.com>
2025-08-21 18:46:19 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function Home() {
       {!user ? (
         <>
 
-          <h1 style="color: red;">こんにちは。autodealです。まずはログインしよう</h1>
+          <h1>こんにちは。autodealです。まずはログインしよう</h1>
           <Link href="/api/auth/login">ログイン</Link>
         </>
       ) : (
