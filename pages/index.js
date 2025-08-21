295165c644d85fb505f95cda6c1edef450345b0d
tukemonogatari6253 <110277953+tukemonogatari6253@users.noreply.github.com>
2025-08-21 21:51:16 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -57,9 +57,9 @@ export default function Home() {
 	<section>
 		<h3>保険サービス</h3>
 		<div class="service">
-			<div class="service-item">
+			<div class="service-item"><Link href="/insurance">会員ページにいく
 				<div class="service-item-img">img</div>
-				<div class="service-item-text">証券サービス</div>
+				<div class="service-item-text">証券サービス</div></Link>
 			</div>
 			<div class="service-item">
 				<div class="service-item-img">img</div>
