52432700d5d305d63d3afb7809bff4aabeadc0a3
tukemonogatari6253 <110277953+tukemonogatari6253@users.noreply.github.com>
2025-08-21 18:59:03 +0900

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 // pages/_app.js
 import { UserProvider } from '@auth0/nextjs-auth0/client';
-
+import './style.css';
 export default function App({ Component, pageProps }) {
   return (
     <UserProvider>
