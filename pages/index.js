8d310e467b03394c0cdd222efe8dc486e65cffaf
tukemonogatari6253 <110277953+tukemonogatari6253@users.noreply.github.com>
2025-08-21 19:04:30 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -33,16 +33,16 @@ export default function Home() {
         </>
       )}
 			 	   <header>
-		<div>ロゴmann</div>
-		<div>
+		<div class="rogo">ロゴmann</div>
+		<div class="header-contents">
 			<ul>
 				<li><Link href="/api/auth/login">サービスとは</Link></li>
 				<li><Link href="/api/auth/login">保険カテゴリ</Link></li>
 				<li><Link href="/api/auth/login">保険会社一覧</Link></li>
 			</ul>
 			<div>
-				<div><Link href="/api/auth/login">新規会員登録</Link></div>
-				<div><Link href="/api/auth/login">ログイン</Link></div>
+				<div class="register"><Link href="/api/auth/login">新規会員登録</Link></div>
+				<div class="login"><Link href="/api/auth/login">ログイン</Link></div>
 			</div>
 		</div>
 		
