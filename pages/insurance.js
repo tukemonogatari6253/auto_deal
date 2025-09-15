7e2ee1bdbc0d0d21aecd3b7dd74d9911a523a741
tukemonogatari6253 <110277953+tukemonogatari6253@users.noreply.github.com>
2025-09-15 17:01:59 +0900

    Update insurance.js

--- pages/insurance.js
+++ pages/insurance.js
@@ -35,21 +35,6 @@ export default function Home({agent,insurance}) {
         </>
       )}
 	<Header />
-			 	   <header>
-		<div class="rogo">ロゴmann</div>
-		<div class="header-contents">
-			<ul>
-				<li><Link href="/api/auth/login">サービスとは</Link></li>
-				<li><Link href="/api/auth/login">保険カテゴリ</Link></li>
-				<li><Link href="/api/auth/login">保険会社一覧</Link></li>
-			</ul>
-			<div class="login-menu">
-				<div class="register"><Link href="/api/auth/login">新規会員登録</Link></div>
-				<div class="login"><Link href="/api/auth/login">ログイン</Link></div>
-			</div>
-		</div>
-		
-	</header>
 	<main>
 	<section>
 		   <div>
