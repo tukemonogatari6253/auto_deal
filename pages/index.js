94b7f4f7eb77065205230aea5a7ee86e4e25baf7
tukemonogatari6253 <110277953+tukemonogatari6253@users.noreply.github.com>
2025-09-15 16:27:25 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import { useUser } from '@auth0/nextjs-auth0/client';
 import Link from 'next/link';
 import Head from 'next/head';
-
+import Header from '../components/Header';
 
 export default function Home() {
   const { user, error, isLoading } = useUser();
@@ -16,37 +16,7 @@ export default function Home() {
         <meta name="description" content="Auth0認証を使ったNext.jsアプリ" />
     <meta name="robots" content="noindex , nofollow" />
       </Head>
-      {!user ? (
-        <>
-
-          <h1>こんにちは。autodealです。まずはログインしよう</h1>
-          <Link href="/api/auth/login">ログイン</Link>
-        </>
-      ) : (
-        <>
-          <h1>ログインできました。こんにちは, {user.name}</h1>
-          <Link href="/api/auth/logout">ログアウト</Link>
-          <br />
-          <Link href="/protected">会員ページにいく</Link>
-          <br />
-          <Link href="/reviews">口コミページへ</Link>
-        </>
-      )}
-			 	   <header>
-		<div class="rogo">ロゴmann</div>
-		<div class="header-contents">
-			<ul>
-				<li><Link href="/api/auth/login">サービスとは</Link></li>
-				<li><Link href="/api/auth/login">保険カテゴリ</Link></li>
-				<li><Link href="/api/auth/login">保険会社一覧</Link></li>
-			</ul>
-			<div class="login-menu">
-				<div class="register"><Link href="/api/auth/login">新規会員登録</Link></div>
-				<div class="login"><Link href="/api/auth/login">ログイン</Link></div>
-			</div>
-		</div>
-		
-	</header>
+	<Header />
 	<main>
 	<section>
 		<h3>サービスの魅力</h3>
