e34ff1004d453d7fdc49871740026d1f3ae49768
tukemonogatari6253 <110277953+tukemonogatari6253@users.noreply.github.com>
2025-09-15 16:31:00 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,7 @@
-import { useUser } from '@auth0/nextjs-auth0/client';
 import Link from 'next/link';
 import Head from 'next/head';
-import Header from '../components/Header';
 
-export default function Home() {
-  const { user, error, isLoading } = useUser();
-
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>{error.message}</div>;
+	import Header from '../components/Header';
 
   return (
     <div>
