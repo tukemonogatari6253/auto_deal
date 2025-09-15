c9225b44531c5cede27f4c9dc0d602c68c8d2570
tukemonogatari6253 <110277953+tukemonogatari6253@users.noreply.github.com>
2025-09-15 16:32:35 +0900

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,4 @@
 import { useUser } from '@auth0/nextjs-auth0/client';
-
-export default function Home() {
   const { user, error, isLoading } = useUser();
 
   if (isLoading) return <div>Loading...</div>;
