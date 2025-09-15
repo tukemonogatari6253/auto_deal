b7e0442316ea914f6eacd313e27172a93ea409c1
tukemonogatari6253 <110277953+tukemonogatari6253@users.noreply.github.com>
2025-09-16 00:15:04 +0900

    Update reviews.js

--- pages/api/reviews.js
+++ pages/api/reviews.js
@@ -3,7 +3,7 @@ import { getSession } from '@auth0/nextjs-auth0';
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
-    const { content, insuranceId } = req.body;
+    const { content, productId } = req.body;
     const session = await getSession(req, res);
     if (!session || !session.user) return res.status(401).end();
 
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
 
     const { error } = await supabase
       .from('reviews')
-      .insert([{ content, user_id: sub, user_name: name, product_id: insuranceId, }]);
+      .insert([{ content, user_id: sub, user_name: name, product_id: productId, },]);
 
     if (error) return res.status(500).json({ error: error.message });
 
