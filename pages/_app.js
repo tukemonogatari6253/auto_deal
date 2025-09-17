7be22edeaba83658ae633a0cc3d874b1afcfaabc
tukemonogatari6253 <110277953+tukemonogatari6253@users.noreply.github.com>
2025-09-17 22:38:29 +0900

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
 // pages/_app.js
 import { UserProvider } from '@auth0/nextjs-auth0/client';
 import './style.css';
+import dayjs from 'dayjs';
+import ja from 'dayjs/locale/ja';
+
+dayjs.locale(ja);
+
 export default function App({ Component, pageProps }) {
   return (
     <UserProvider>
