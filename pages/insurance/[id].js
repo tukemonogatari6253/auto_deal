ebec408091075f72c2bac55d833d484ca8e3ce0f
tukemonogatari6253 <110277953+tukemonogatari6253@users.noreply.github.com>
2025-09-17 22:32:15 +0900

    Update [id].js

--- pages/insurance/[id].js
+++ pages/insurance/[id].js
@@ -6,6 +6,20 @@ import Head from 'next/head';
 
 import { useState, useEffect } from 'react';
 
+import dayjs from "dayjs";
+import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+export const formatCommon =
+  (format: string) =>
+  (date: string | Date | number): string => {
+    return dayjs.utc(date).tz("Asia/Tokyo").format(format) as string;
+  };
+
+export const formatDate = formatCommon("YYYY年MM月DD日 hh:mm");
+
 export default function insuranceId({ insurance }){
   const { user, error, isLoading } = useUser();
 
