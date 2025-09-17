db7bd63aaff4e54e908ef7805b95f58c37636f9a
tukemonogatari6253 <110277953+tukemonogatari6253@users.noreply.github.com>
2025-09-17 23:43:43 +0900

    Update [id].js

--- pages/insurance/[id].js
+++ pages/insurance/[id].js
@@ -120,14 +120,14 @@ export default function insuranceId({ insurance }){
 			</div>
 			<div class="review-items">
 		        {reviews.map((r) => (
-   			    <div key={r.id}>
+   			    <div key={r.id}  class="review">
 					<div class="review-item">
 	        	    	<div class="review-item-img"><img src={insurance.icon.url} /></div>
 						<div class="review-item-text">
 							 <strong>{r.user_name}　{formatCommon("YYYY年MM月DD日")(r.created_at)}</strong>
 						</div>
-					{r.content}
 					</div>
+					{r.content}
   		        </div>
        			 ))}
 			{user && (
