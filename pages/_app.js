c7f254342bd29cccd774564dc7aef55cebaef8bb
tukemonogatari6253 <110277953+tukemonogatari6253@users.noreply.github.com>
2025-09-17 23:01:47 +0900

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,21 @@
 import { UserProvider } from '@auth0/nextjs-auth0/client';
 import './style.css';
 import dayjs from 'dayjs';
-import ja from 'dayjs/locale/ja';
+import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+export const formatCommon =
+  (format: string) =>
+  (date: string | Date | number): string => {
+    return dayjs.utc(date).tz("Asia/Tokyo").format(format) as string;
+  };
+
 
 dayjs.locale(ja);
-console.log(dayjs());
+console.log(formatCommon(new Date));
 
 export default function App({ Component, pageProps }) {
   return (
